2574f43d65d2c68734f9ca5f3316c2f7eb008377
Younes Bouhouche <younes.bouhouche12@gmail.com>
2026-06-10 16:11:14 +0100

    refactor: Remove unused imports

--- app/src/main/java/com/younesb/securevault/features/auth/presentation/screens/AuthPinScreen.kt
+++ app/src/main/java/com/younesb/securevault/features/auth/presentation/screens/AuthPinScreen.kt
@@ -32,7 +32,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
