cccf753215a0e6a7edb78cab01601a3275cc3634
Younes Bouhouche <younes.bouhouche12@gmail.com>
2026-06-13 14:54:26 +0100

    refactor: Introduce new result wrapper for better error handling

--- app/src/main/java/com/younesb/securevault/features/main/data/repository/DocumentsRepositoryImpl.kt
+++ app/src/main/java/com/younesb/securevault/features/main/data/repository/DocumentsRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package com.younesb.securevault.features.main.data.repository
 
+import com.younesb.securevault.core.domain.utils.EmptyResult
+import com.younesb.securevault.core.domain.utils.Result
 import com.younesb.securevault.features.main.data.datasource.local.database.dao.DocumentsDao
 import com.younesb.securevault.features.main.data.datasource.local.database.models.Document
 import com.younesb.securevault.features.main.data.datasource.local.database.models.DocumentTagCrossRef
@@ -15,47 +17,55 @@ import java.util.UUID
 
 class DocumentsRepositoryImpl(
     val dao: DocumentsDao
-): DocumentsRepository {
+) : DocumentsRepository {
     override fun observeFolders(): Flow<List<FolderDto>> =
         dao.observeFoldersWithDocuments().map {
             it.map(FolderDto::fromFolderWithDocuments)
         }
 
-    override suspend fun getFolders(): List<FolderDto> =
-        dao.getFoldersWithDocuments().map(FolderDto::fromFolderWithDocuments)
+    override suspend fun getFolders(): Result<List<FolderDto>, Exception> =
+        Result.run {
+            dao.getFoldersWithDocuments().map(FolderDto::fromFolderWithDocuments)
+        }
 
-    override suspend fun createFolder(name: String): FolderDto =
-        FolderDto.fromFolder(
-            dao.upsertFolder(
-                Folder(
-                    id = UUID.randomUUID().toString(),
-                    name = name,
-                    createdAt = System.currentTimeMillis()
+    override suspend fun createFolder(name: String): Result<FolderDto, Exception> =
+        Result.run {
+            FolderDto.fromFolder(
+                dao.upsertFolder(
+                    Folder(
+                        id = UUID.randomUUID().toString(),
+                        name = name,
+                        createdAt = System.currentTimeMillis()
+                    )
                 )
             )
-        )
+        }
 
-    override suspend fun deleteFolder(folderId: String, deleteDocuments: Boolean) {
+    override suspend fun deleteFolder(
+        folderId: String,
+        deleteDocuments: Boolean
+    ): EmptyResult<Exception> = Result.run {
         dao.deleteFolder(folderId)
         if (deleteDocuments) dao.deleteDocumentsByFolderId(folderId)
     }
 
-    override suspend fun renameFolder(folderId: String, newName: String) {
-        val folder = dao.getFolderById(folderId)?.folder
-            ?: throw IllegalArgumentException("Folder with id $folderId not found")
-        dao.upsertFolder(folder.copy(name = newName))
-    }
+    override suspend fun renameFolder(folderId: String, newName: String): EmptyResult<Exception> =
+        Result.run {
+            val folder = dao.getFolderById(folderId)?.folder
+                ?: throw IllegalArgumentException("Folder with id $folderId not found")
+            dao.upsertFolder(folder.copy(name = newName))
+        }
 
     override fun observeDocuments(): Flow<List<DocumentDto>> =
         dao.observeDocumentsWithTags().map {
             it.map(DocumentDto::fromDocumentWithTags)
         }
 
-    override suspend fun getDocuments(): List<DocumentDto> {
-        return dao.getDocumentsWithTags().map(DocumentDto::fromDocumentWithTags)
+    override suspend fun getDocuments(): Result<List<DocumentDto>, Exception> = Result.run {
+        dao.getDocumentsWithTags().map(DocumentDto::fromDocumentWithTags)
     }
 
-    override suspend fun createDocument(request: DocumentDto) {
+    override suspend fun createDocument(request: DocumentDto): EmptyResult<Exception> = Result.run {
         dao.upsertDocument(
             Document(
                 id = UUID.randomUUID().toString(),
@@ -74,27 +84,40 @@ class DocumentsRepositoryImpl(
             ?: throw IllegalArgumentException("Document with id $documentId not found")
     }
 
-    override suspend fun deleteDocument(documentId: String) {
+    override suspend fun deleteDocument(documentId: String): EmptyResult<Exception> = Result.run {
         dao.deleteDocument(getDocumentById(documentId))
     }
 
-    override suspend fun renameDocument(documentId: String, newName: String) {
+    override suspend fun renameDocument(
+        documentId: String,
+        newName: String
+    ): EmptyResult<Exception> = Result.run {
         val document = getDocumentById(documentId)
-        dao.upsertDocument(document.copy(
-            name = newName,
-            updatedAt = System.currentTimeMillis()
-        ))
+        dao.upsertDocument(
+            document.copy(
+                name = newName,
+                updatedAt = System.currentTimeMillis()
+            )
+        )
     }
 
-    override suspend fun moveDocument(documentId: String, newFolderId: String?) {
+    override suspend fun moveDocument(
+        documentId: String,
+        newFolderId: String?
+    ): EmptyResult<Exception> = Result.run {
         val document = getDocumentById(documentId)
-        dao.upsertDocument(document.copy(
-            folderId = newFolderId,
-            updatedAt = System.currentTimeMillis()
-        ))
+        dao.upsertDocument(
+            document.copy(
+                folderId = newFolderId,
+                updatedAt = System.currentTimeMillis()
+            )
+        )
     }
 
-    override suspend fun addTagToDocument(documentId: String, tagId: String) {
+    override suspend fun addTagToDocument(
+        documentId: String,
+        tagId: String
+    ): EmptyResult<Exception> = Result.run {
         dao.insertDocumentTagCrossRef(
             DocumentTagCrossRef(
                 documentId = documentId,
@@ -103,7 +126,10 @@ class DocumentsRepositoryImpl(
         )
     }
 
-    override suspend fun removeTagFromDocument(documentId: String, tagId: String) {
+    override suspend fun removeTagFromDocument(
+        documentId: String,
+        tagId: String
+    ): EmptyResult<Exception> = Result.run {
         dao.deleteDocumentTagCrossRef(
             DocumentTagCrossRef(
                 documentId = documentId,
@@ -117,10 +143,12 @@ class DocumentsRepositoryImpl(
             it.map(TagDto::fromTag)
         }
 
-    override suspend fun getTags(): List<TagDto> =
-        dao.getTags().map(TagDto::fromTag)
+    override suspend fun getTags(): Result<List<TagDto>, Exception> =
+        Result.run {
+            dao.getTags().map(TagDto::fromTag)
+        }
 
-    override suspend fun createTag(name: String){
+    override suspend fun createTag(name: String): EmptyResult<Exception> = Result.run {
         dao.upsertTag(
             Tag(
                 id = UUID.randomUUID().toString(),
@@ -130,7 +158,7 @@ class DocumentsRepositoryImpl(
         )
     }
 
-    override suspend fun deleteTag(tagId: String) {
+    override suspend fun deleteTag(tagId: String): EmptyResult<Exception> = Result.run {
         val tag = dao.getTagById(tagId)
             ?: throw IllegalArgumentException("Tag with id $tagId not found")
         dao.deleteTag(tag)
--- app/src/main/java/com/younesb/securevault/features/main/domain/repository/DocumentsRepository.kt
+++ app/src/main/java/com/younesb/securevault/features/main/domain/repository/DocumentsRepository.kt
@@ -1,5 +1,7 @@
 package com.younesb.securevault.features.main.domain.repository
 
+import com.younesb.securevault.core.domain.utils.EmptyResult
+import com.younesb.securevault.core.domain.utils.Result
 import com.younesb.securevault.features.main.domain.models.DocumentDto
 import com.younesb.securevault.features.main.domain.models.FolderDto
 import com.younesb.securevault.features.main.domain.models.TagDto
@@ -7,22 +9,22 @@ import kotlinx.coroutines.flow.Flow
 
 interface DocumentsRepository {
     fun observeFolders(): Flow<List<FolderDto>>
-    suspend fun getFolders(): List<FolderDto>
-    suspend fun createFolder(name: String): FolderDto
-    suspend fun deleteFolder(folderId: String, deleteDocuments: Boolean = false)
-    suspend fun renameFolder(folderId: String, newName: String)
+    suspend fun getFolders(): Result<List<FolderDto>, Exception>
+    suspend fun createFolder(name: String): Result<FolderDto, Exception>
+    suspend fun deleteFolder(folderId: String, deleteDocuments: Boolean = false): EmptyResult<Exception>
+    suspend fun renameFolder(folderId: String, newName: String): EmptyResult<Exception>
 
     fun observeDocuments(): Flow<List<DocumentDto>>
-    suspend fun getDocuments(): List<DocumentDto>
-    suspend fun createDocument(request: DocumentDto)
-    suspend fun deleteDocument(documentId: String)
-    suspend fun renameDocument(documentId: String, newName: String)
-    suspend fun moveDocument(documentId: String, newFolderId: String?)
-    suspend fun addTagToDocument(documentId: String, tagId: String)
-    suspend fun removeTagFromDocument(documentId: String, tagId: String)
+    suspend fun getDocuments(): Result<List<DocumentDto>, Exception>
+    suspend fun createDocument(request: DocumentDto): EmptyResult<Exception>
+    suspend fun deleteDocument(documentId: String): EmptyResult<Exception>
+    suspend fun renameDocument(documentId: String, newName: String): EmptyResult<Exception>
+    suspend fun moveDocument(documentId: String, newFolderId: String?): EmptyResult<Exception>
+    suspend fun addTagToDocument(documentId: String, tagId: String): EmptyResult<Exception>
+    suspend fun removeTagFromDocument(documentId: String, tagId: String): EmptyResult<Exception>
 
     fun observeTags(): Flow<List<TagDto>>
-    suspend fun getTags(): List<TagDto>
-    suspend fun createTag(name: String)
-    suspend fun deleteTag(tagId: String)
+    suspend fun getTags(): Result<List<TagDto>, Exception>
+    suspend fun createTag(name: String): EmptyResult<Exception>
+    suspend fun deleteTag(tagId: String): EmptyResult<Exception>
 }
