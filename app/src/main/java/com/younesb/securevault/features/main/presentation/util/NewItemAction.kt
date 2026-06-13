ba0320355e98035149af1fbe686f046b8fa8baa0
Younes Bouhouche <younes.bouhouche12@gmail.com>
2026-06-13 20:08:49 +0100

    feat: Add gallery option to NewItemAction

--- app/src/main/java/com/younesb/securevault/features/main/presentation/util/NewItemAction.kt
+++ app/src/main/java/com/younesb/securevault/features/main/presentation/util/NewItemAction.kt
@@ -4,6 +4,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.Note
 import androidx.compose.material.icons.rounded.CameraAlt
 import androidx.compose.material.icons.rounded.Download
+import androidx.compose.material.icons.rounded.PhotoLibrary
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.younesb.securevault.R
 
@@ -13,5 +14,6 @@ enum class NewItemAction(
 ) {
     IMPORT(Icons.Rounded.Download, R.string._import),
     CAMERA(Icons.Rounded.CameraAlt, R.string.camera),
+    GALLERY(Icons.Rounded.PhotoLibrary, R.string.gallery),
     NOTE(Icons.AutoMirrored.Rounded.Note, R.string.note)
 }
