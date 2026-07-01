946be41acfb8fb06348c7f59c4a77591abe046cc
Younes Bouhouche <younes.bouhouche12@gmail.com>
2026-07-01 09:35:35 +0100

    refactor: Extract AnimatedIconContainer to use it everywhere

--- app/src/main/java/com/younesb/securevault/core/presentation/components/AnimatedIconContainer.kt
+++ app/src/main/java/com/younesb/securevault/core/presentation/components/AnimatedIconContainer.kt
@@ -1,11 +1,9 @@
-package com.younesb.securevault.features.auth.presentation.components
+package com.younesb.securevault.core.presentation.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LoadingIndicator
@@ -15,14 +13,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
-fun AnimatedAppLogo(modifier: Modifier = Modifier) {
+fun AnimatedIconContainer(
+    icon: ImageVector,
+    modifier: Modifier = Modifier,
+    containerColor: Color = MaterialTheme.colorScheme.onBackground,
+    color: Color = MaterialTheme.colorScheme.background,
+) {
     Box(modifier.fillMaxWidth().aspectRatio(1f), contentAlignment = Alignment.Center) {
         LoadingIndicator(
             modifier = Modifier.fillMaxSize().scale(1.2f),
-            color = MaterialTheme.colorScheme.onBackground,
+            color = containerColor,
             polygons = listOf(
                 MaterialShapes.Sunny,
                 MaterialShapes.VerySunny,
@@ -31,10 +36,10 @@ fun AnimatedAppLogo(modifier: Modifier = Modifier) {
             ),
         )
         Icon(
-            Icons.Default.Lock,
+            icon,
             null,
             modifier = Modifier.fillMaxSize(.3f),
-            tint = MaterialTheme.colorScheme.background
+            tint = color
         )
     }
 }
