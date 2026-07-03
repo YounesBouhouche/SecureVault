8e79941da32661f80d42908858a9a9e40d4e1bf2
Younes Bouhouche <younes.bouhouche12@gmail.com>
2026-07-03 09:10:30 +0100

    feat: Add app logo

--- app/src/main/java/com/younesb/securevault/core/presentation/components/AnimatedAppLogo.kt
+++ app/src/main/java/com/younesb/securevault/core/presentation/components/AnimatedAppLogo.kt
@@ -1,12 +1,13 @@
 package com.younesb.securevault.core.presentation.components
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.younesb.securevault.R
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun AnimatedAppLogo(modifier: Modifier = Modifier) =
-    AnimatedIconContainer(Icons.Default.Lock, modifier)
+    AnimatedIconContainer(ImageVector.vectorResource(R.drawable.app_logo), modifier)
